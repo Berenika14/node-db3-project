b498f249f8dcac6d70afb8698164fb76f05223f1
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-16 23:03:20 -0500

    wrote middleware for validating schemename and instructions and stepnumber

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -18,6 +18,8 @@ const checkSchemeId = async (req, res, next) => {
         status: 404,
         message: `scheme with scheme_id ${req.params.scheme_id} not found`,
       });
+    } else {
+      next();
     }
   } catch (err) {
     next(err);
@@ -32,7 +34,18 @@ const checkSchemeId = async (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {};
+const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body;
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name != "string" ||
+    !scheme_name.trim()
+  ) {
+    next({ status: 400, message: "invalid scheme_name" });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -43,7 +56,20 @@ const validateScheme = (req, res, next) => {};
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {};
+const validateStep = (req, res, next) => {
+  const { instructions, step_number } = req.body;
+  if (
+    instructions === undefined ||
+    typeof instructions != "string" ||
+    !instructions ||
+    typeof step_number != "number" ||
+    step_number < 1
+  ) {
+    next({ status: 400, message: "invalid step" });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
