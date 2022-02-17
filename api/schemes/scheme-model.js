d3137603f74128f001e324e32c7655c6c530733c
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-17 00:46:09 -0500

    wrote findsteps function

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -119,7 +119,7 @@ async function findById(scheme_id) {
   return result;
 }
 
-function findSteps(scheme_id) {
+async function findSteps(scheme_id) {
   // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
@@ -141,6 +141,18 @@ function findSteps(scheme_id) {
         }
       ]
   */
+  //   SELECT
+  //   step_id, step_number, instructions, scheme_name  from schemes as sc
+  //  left join steps as st on sc.scheme_id= st.scheme_id where sc.scheme_id =1
+  //  order by step_number;
+
+  const rows = await db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .select("st.step_id", "st.step_number", "instructions", "sc.scheme_name")
+    .where("sc.scheme_id", scheme_id)
+    .orderBy("st.step_number");
+  if (!rows[0].step_id) return [];
+  return rows;
 }
 
 function add(scheme) {
